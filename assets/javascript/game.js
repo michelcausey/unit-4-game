32f6b41ec24921efed6243acd3dcf5f860193412
MiChel Causey <michel.causey@gmail.com>
2018-09-16 14:39:34 -0400

    empty array and totalScore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,58 @@
-
+var wins = 0
+var losses = 0
 var targetNumber = $("#target-number")
 var totalScore = $("#total-score")
+var wins = $("#wins")
+var losses = $("#losses")
+
+// an array to hold our crystal values
+var randomNumberArray = []
 
 // start the game & display a random TARGET NUMBER between 19 - 120
+// used 102 + 19 because FLOOR = 0 + 19
+// used 102 + 19 to get highest number at 120
 
-function startGame() {
-    targetNumber = Math.floor(Math.random() * 102) + 19
-    console.log (targetNumber)
-    $("#target-number").html("The target number is: "+ targetNumber)
+function startGame(){
+targetNumber = Math.floor(Math.random() * 102) + 19
+console.log("Target Number: " + targetNumber)
+$("#target-number").html("The target number is: " + targetNumber)
+
+$("#crystal-image").on("click", function () {
+    console.log("You clicked a crystal")
+    console.log(crystalValue)
+});
+
+// each crystal gets a value of 12 for the game / resets after win/loss
+for (var i = 0; i < 4; i++) {
+    var crystalValue = Math.floor(Math.random() * 11) + 1;
+    console.log(crystalValue);
+
+    var newImage = $("<img>");
+    newImage.attr({
+        "id": "crystal-image",
+        "src": "./assets/images/crystal1.jpg",
+        "data": crystalValue
+    })
+
+    $("#crystal-image").append(newImage)
+
+    randomNumberArray.push(crystalValue)
+    console.log(randomNumberArray)
+
+    totalScore = (randomNumberArray[0] + randomNumberArray[1] + randomNumberArray[2] + randomNumberArray[3])
+    console.log(totalScore)
+
+    $("#total-score").html("Your Total Score is: " + totalScore)
+};
 }
 
-// add "on click" events to each crystal between 1 - 12
+startGame()
 
-$("#crystal-1").on("click", function() {
-    var crystalValue1 = Math.floor(Math.random() * 12) + 1
-    console.log("the value of crystal 1 is: " + crystalValue1)
-});
+// add "on click" events to crystal between 1 - 12
 
-$("#crystal-2").on("click", function() {
-    var crystalValue2 = Math.floor(Math.random() * 12) + 1
-    console.log("the value of crystal 2 is: " + crystalValue2)
-});
 
-$("#crystal-3").on("click", function() {
-    var crystalValue3 = Math.floor(Math.random() * 12) + 1
-    console.log("the value of crystal 3 is: " + crystalValue3)
-});
 
-$("#crystal-4").on("click", function() {
-    var crystalValue4 = Math.floor(Math.random() * 12) + 1
-    console.log("the value of crystal 4 is: " + crystalValue4)
 
-});
 
 // player clicks a crystal, & random value is added to SCORE
 
@@ -42,5 +63,3 @@ $("#crystal-4").on("click", function() {
 // re-set the game when a player wins or loses
 
 // at re-set crystal gets a new value assigned, but keep wins/losses displayed
-
-startGame();
