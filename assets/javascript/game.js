4f4f7de4aca00e6cefd3e411754d926137588731
MiChel Causey <michel.causey@gmail.com>
2018-09-19 22:08:32 -0400

    correct function to add up total score. changed styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,75 +2,78 @@ var wins = 0
 var losses = 0
 var targetNumber = $("#target-number")
 var totalScore = 0
+var counter = 0
 
-var wins = $("#wins")
-var losses = $("#losses")
-
-// an array to hold our crystal values
-var randomNumberArray = []
-
-function startGame(){
-targetNumber = Math.floor(Math.random() * 102) + 19
-console.log("Target Number: " + targetNumber)
-$("#target-number").html("The target number is: " + targetNumber)
+function startGame() {
+    targetNumber = Math.floor(Math.random() * 102) + 19
+    console.log("Target Number: " + targetNumber)
+    $("#target-number").html("The target number is: " + targetNumber)
 
 $("#crystal-1").on("click", function () {
     console.log("You clicked crystal 1")
     var cry1Score = Math.floor(Math.random() * 12) + 1
-    randomNumberArray.push(cry1Score)
-    console.log (cry1Score)
-    console.log(randomNumberArray)
+    console.log(cry1Score);
+    counter += cry1Score;
+    console.log(counter);
+    totalScore();
 });
 
 $("#crystal-2").on("click", function () {
-    console.log("You clicked crystal 2")
+    console.log("You clicked crystal 1")
     var cry2Score = Math.floor(Math.random() * 12) + 1
-    console.log (cry2Score)
-    randomNumberArray.push(cry2Score)
-    console.log(randomNumberArray)
+    console.log(cry2Score);
+    counter += cry2Score;
+    console.log(counter);
+    totalScore();
+
 });
 
 $("#crystal-3").on("click", function () {
     console.log("You clicked crystal 3")
     var cry3Score = Math.floor(Math.random() * 12) + 1
-    console.log (cry3Score)
-    randomNumberArray.push(cry3Score)
-    console.log(randomNumberArray)
+    console.log(cry3Score);
+    counter += cry3Score;
+    console.log(counter);
+    totalScore();
 });
 
 $("#crystal-4").on("click", function () {
     console.log("You clicked crystal 4")
     var cry4Score = Math.floor(Math.random() * 12) + 1
-    console.log (cry4Score)
-    randomNumberArray.push(cry4Score)
-    console.log(randomNumberArray)
+    console.log(cry4Score);
+    counter += cry4Score;
+    console.log(counter);
+    totalScore();
 });
-
-$("#total-score").html("Your total score is: " + totalScore)
-console.log(totalScore)
-};
-
-startGame()
-
-
-
-// player clicks a crystal, & random value is added to SCORE
-
-// player wins if their score equals the random number - alert ("win") - update wins
-
-if (totalScore === targetNumber) {
-    wins++;
-    alert("You Win!")
-    $("#wins").html("Games Won: " + wins)
-} else if (totalScore >= targetNumber) {
-    losses++;
-    alert("Game Over - You Lose")
-    $("#losses").html("Games Lost: " + losses)
-
 }
 
-// player loses if their score exceeds the random number - alert ("lose") - update losses
+var reset = function (){
+    counter = 0
+    targetNumber = Math.floor(Math.random() * 102) + 19
+    console.log("Target Number: " + targetNumber)
+    $("#target-number").html("The target number is: " + targetNumber)
+}
 
-// re-set the game when a player wins or loses
+var totalScore = function() {
+    $("#total-score").html("Your current score is: " + counter);
 
-// at re-set crystal gets a new value assigned, but keep wins/losses displayed
+    if (counter === targetNumber) {
+        alert("Woohoo! You Won!");
+        reset()
+        $("#total-score").html("Your current score is: 0" )
+        wins++
+        console.log(wins)
+        $("#wins").html("Wins: " + wins)
+    } else if (counter > targetNumber) {
+        alert("Game Over! You Lost")
+        reset()
+        $("#total-score").html("Your current score is: 0" )
+        losses++
+        console.log(losses)
+        $("#losses").html("Losses: " + losses)
+    }
+
+}
+startGame()
+
+// reset function that re-sets everything but the wins + losses
